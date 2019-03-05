9e22e91f4307038942bf72b9a30091c816b54ef9
Angus <angushon96@gmail.com>
2019-03-05 01:01:10 -0800

    same overlay as menu but for cart

--- js/krabbypatty.js
+++ js/krabbypatty.js
@@ -1,10 +1,18 @@
 // helped with what i wanted to do https://jsfiddle.net/kym2rvyL/1/
 window.addEventListener("click", function(e){
+  //menu
   if(document.getElementById("toggle").contains(e.target)){
     document.getElementById("menu_stuff").style.width = "40%";
   }
   else if(!document.getElementById("menu_items").contains(e.target)){
     // if not clicking inside menu, close menu
     document.getElementById("menu_stuff").style.width = "0%";
+  }
+
+  //cart menu
+  if(document.getElementById("toggle_cart").contains(e.target)){
+    document.getElementById("menu_cart_stuff").style.width = "25%";
+  }else{
+    document.getElementById("menu_cart_stuff").style.width = "0%";
   }
 });
