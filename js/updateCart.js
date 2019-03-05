a58b992f96f3491abea6f336fbc95519292bf5df
Angus <angushon96@gmail.com>
2019-03-05 03:35:21 -0800

    made sure stuff works through multiple pages
    added descriptions for my js functions
    
    added cart into mens.html

--- js/updateCart.js
+++ js/updateCart.js
@@ -1,16 +1,23 @@
 window.onload = function () {
+  //check if its first load, if it is then set values
   if (localStorage.getItem("firstLoad") === null) {
     localStorage.setItem("firstLoad", 1);
     localStorage.setItem("cartNum", 0);
     console.log(localStorage.getItem("cartNum"));
   } else {
+    //load in number of cart items
     document.getElementById("cart_label").innerHTML = "Cart(" + localStorage.getItem("cartNum") + ")";
+    //if num of cart items is > 0, load them in
+    for(var i = 0; i < localStorage.getItem("cartNum"); i++){
+      appendHtml(document.getElementById("scroll_section"), item);
+    }
   }
 }
 
 var item = '<div class="cart_listing" id="listing"><img src="https://via.placeholder.com/150" alt="placeholder"><div class="cart_item_description"><h3>Item Name</h3><p>item description</p></div></div>'
 
 function update() {
+  //update cartNum
   if (localStorage.getItem("cartNum")) {
     var temp = localStorage.getItem("cartNum");
     var tempInt = parseInt(temp);
@@ -18,10 +25,12 @@ function update() {
     console.log(tempInt);
     localStorage.setItem("cartNum", tempInt);
   }
+  //add html chunk
   document.getElementById("cart_label").innerHTML = "Cart(" + localStorage.getItem("cartNum") + ")";
   appendHtml(document.getElementById("scroll_section"), item);
 }
 
+//reset the value and remove all existing cart items
 function resetValue() {
   localStorage.setItem("cartNum", 0);
   console.log(localStorage.getItem("cartNum"));
